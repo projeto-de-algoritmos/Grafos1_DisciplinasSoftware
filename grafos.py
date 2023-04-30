d70c67c22308bbed3d32b67436af42a22097a23d
AMANDA FERNANDES CUSTODIO <58089751+amanda-noda@users.noreply.github.com>
2023-04-29 22:21:30 -0300

    edit: Adiciona grafo direcionado

--- grafos.py
+++ grafos.py
@@ -10,7 +10,25 @@ class Grafo:
             self.graph[v2] = []
         self.graph[v1].append(v2)
         #self.graph[v2].append(v1)
-    
+   
+    def adiciona_arco(self, u, v):
+        " Adiciona uma ligação (arco) entre os nodos 'u' e 'v'. "
+        self.adj[u].add(v)
+        # Se o grafo é não-direcionado, precisamos adicionar arcos nos dois sentidos.
+        if not self.direcionado:
+            self.adj[v].add(u)
+
+    def __len__(self):
+        return len(self.adj)
+
+
+    def __str__(self):
+        return '{}({})'.format(self.__class__.__name__, dict(self.adj))
+
+
+    def __getitem__(self, v):
+        return self.adj[v]
+
     def printa_grafo(self):
         for v in self.graph:
             if self.graph[v]:
